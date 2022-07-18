4476185298fc24f00f50e47d79356bb5df0cda82
cosmic-bombay <95729008+cosmic-bombay@users.noreply.github.com>
2022-07-18 17:51:49 -0600

    Begin project

---
+++ src/Components/TrackList/TrackList.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import './TrackList.css';
+
+
+class Track extends React.Component {
+  render() {
+    return (
+      <div className="TrackList">
+      <p>Map of track components</p>
+      </div>
+    )
+  }
+}
+
+export default TrackList;
